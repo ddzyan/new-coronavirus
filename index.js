ee29988435f59cb9968e7f0ef57e2e1368882d91
dingdz <ddzyan@163.com>
2020-02-07 15:26:56 +0800

    将日志输出到本地文件中，代码使用crontab管理

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const NewCoronavirusReptilia = require('./lib');
 const logger = require('./utils/Logger')();
 
-/*
-* TODO
-* 1.使用crontab 定时任务
-* 2.采用logger将控制台信息输出带本地日志文件中
-*/
-
 const time = process.argv[2] || 30;
-console.log(`设定任务间隔时间为${time}分钟`);
+logger.debug(`设定任务间隔时间为${time}分钟`);
 // TODO 判断数据库连接是否正常
-let count = 1;
 const startTime = new Date();
 // 病毒爬虫任务
 function newCoronavirusJob() {
   NewCoronavirusReptilia.saveNewCoronavirusDate().then(() => {
-    logger.debug(`第${count}次数据获取完成,消耗时间为${new Date() - startTime}ms`);
+    logger.debug(`数据获取完成,消耗时间为${new Date() - startTime}ms`);
   }).catch((err) => {
-    console.error('newCoronavirusJob', err);
-  }).finally(() => {
-    count++;
+    logger.error('newCoronavirusJob', err);
   });
 }
 // 启动时，马上获取一次
 newCoronavirusJob();
 // 启动定时任务
-setInterval(newCoronavirusJob, Number.parseInt(time) * 60 * 1000);
+// setInterval(newCoronavirusJob, Number.parseInt(time) * 60 * 1000);
